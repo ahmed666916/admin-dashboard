8e942c3f045a4780170ef7e98bde98e2ede3eb06
ahmed666916 <ahmed666916@gmail.com>
2024-02-23 20:15:17 +0500

    added button icons for the projects

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+    
+    icon1 = "../images/star.svg";
+    icon2 = "../images/eye.svg";
+    icon3 = "../images/share.svg";
     // Data for the cards
     const cardData = [
-        { heading: "Super Cool Project 1", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: ["icon1.png", "icon2.png", "icon3.png"] },
-        { heading: "Super Cool Project 2", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: ["icon1.png", "icon2.png", "icon3.png"] },
-        { heading: "Super Cool Project 3", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: ["icon1.png", "icon2.png", "icon3.png"] },
-        { heading: "Super Cool Project 4", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: ["icon1.png", "icon2.png", "icon3.png"] },
-        { heading: "Super Cool Project 5", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: ["icon1.png", "icon2.png", "icon3.png"] },
-        { heading: "Super Cool Project 6", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: ["icon1.png", "icon2.png", "icon3.png"] }
+        { heading: "Super Cool Project 1", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: [icon1, icon2, icon3] },
+        { heading: "Super Cool Project 2", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: [icon1, icon2, icon3] },
+        { heading: "Super Cool Project 3", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: [icon1, icon2, icon3] },
+        { heading: "Super Cool Project 4", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: [icon1, icon2, icon3] },
+        { heading: "Super Cool Project 5", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: [icon1, icon2, icon3] },
+        { heading: "Super Cool Project 6", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", icons: [icon1, icon2, icon3] }
     ];
     
     // Function to create a card element
